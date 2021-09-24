da1cff82f787c209904fb730f63082ec017d9459
K-Yonhon <k.yonhon@gmail.com>
2021-09-24 23:45:41 +0900

    Model works well

--- Assets/Env_old.cs
+++ Assets/Env_old.cs
@@ -7,12 +7,12 @@ using UnityEngine.Serialization;
 //
 
 
-public class Env : MonoBehaviour
+public class Env_old : MonoBehaviour
 {
     public GameObject HumanoidModel;
     
     const int num_objects = 17;
-    const int num_joints = 11;
+    const int num_joints = 17;
     const int dim_states = 8 + num_joints*2 + 2;
     const int dim_actions = num_joints;
 
@@ -100,10 +100,10 @@ public class Env : MonoBehaviour
         game_objects[4] = HumanoidModel.transform.Find( "butt" ).gameObject;
         game_objects[5] = HumanoidModel.transform.Find( "right_thigh1" ).gameObject;
         game_objects[6] = HumanoidModel.transform.Find( "right_shin1" ).gameObject;
-        game_objects[7] = game_objects[6].transform.Find( "right_foot" ).gameObject;
+        game_objects[7] = HumanoidModel.transform.Find( "right_foot" ).gameObject;
         game_objects[8] = HumanoidModel.transform.Find( "left_thigh1" ).gameObject;
         game_objects[9] = HumanoidModel.transform.Find( "left_shin1" ).gameObject;
-        game_objects[10] = game_objects[9].transform.Find( "left_foot" ).gameObject;
+        game_objects[10] = HumanoidModel.transform.Find( "left_foot" ).gameObject;
         game_objects[11] = HumanoidModel.transform.Find( "right_uarm1" ).gameObject;
         game_objects[12] = HumanoidModel.transform.Find( "right_larm" ).gameObject;
         game_objects[13] = HumanoidModel.transform.Find( "right_hand" ).gameObject;
@@ -127,35 +127,34 @@ public class Env : MonoBehaviour
         HingeJoint[] LeftUarm1_Joint = game_objects[14].GetComponents<HingeJoint>();
         HingeJoint[] LeftLarm_Joint = game_objects[15].GetComponents<HingeJoint>();
 
-        indexed_joints[0] = Lwaist_Joint[0];         // y -30 75   y-1 -75 30
-        indexed_joints[1] = Lwaist_Joint[1];         // z -45 45       -45 45
-        indexed_joints[2] = Butt_Joint[0];           // x -35 35       -35 35
+        indexed_joints[0] = Lwaist_Joint[0];
+        indexed_joints[1] = Lwaist_Joint[1];    
+        indexed_joints[2] = Butt_Joint[0];
 
-        indexed_joints[3] = RightThigh1_Joint[0];    //x- -25  5               
-        // indexed_joints[4] = RightThigh1_Joint[1]; //y+ -35  60       
-        // indexed_joints[5] = RightThigh1_Joint[2]; //z- -120 20      -120 20
+        indexed_joints[3] = RightThigh1_Joint[0];
+        indexed_joints[4] = RightThigh1_Joint[1];
+        indexed_joints[5] = RightThigh1_Joint[2];  
 
-        indexed_joints[4] = RightShin1_Joint[0];     //z+ -2  160  z-1 -160 2
+        indexed_joints[6] = RightShin1_Joint[0];
 
-        indexed_joints[5] = LeftThigh1_Joint[0];     //x+ -5   25        
-        // indexed_joints[8] = LeftThigh1_Joint[1];  //y- -60  35
-        // indexed_joints[9] = LeftThigh1_Joint[2];  //z- -120 20      -120 20
+        indexed_joints[7] = LeftThigh1_Joint[0];
+        indexed_joints[8] = LeftThigh1_Joint[1];
+        indexed_joints[9] = LeftThigh1_Joint[2];
 
-        indexed_joints[6] = LeftShin1_Joint[0];      //z+ -2  160  z-1 -160 2
+        indexed_joints[10] = LeftShin1_Joint[0];
 
-        indexed_joints[7] = RightUarm1_Joint[0];     //x- -85  60
-        // indexed_joints[12] = RightUarm1_Joint[1]; //z- -85  60      -85 60
-        indexed_joints[8] = RightLarm_Joint[0];      //x  -90  50      -90 50
+        indexed_joints[11] = RightUarm1_Joint[0];
+        indexed_joints[12] = RightUarm1_Joint[1];
+        indexed_joints[13] = RightLarm_Joint[0];
 
-        indexed_joints[9] = LeftUarm1_Joint[0];      //x+ -60  85
-        // indexed_joints[15] = LeftUarm1_Joint[1];  //z- -85  60      -85 60
-        indexed_joints[10] = LeftLarm_Joint[0];      //x  -50  90  x-1 -90 50
-
+        indexed_joints[14] = LeftUarm1_Joint[0];
+        indexed_joints[15] = LeftUarm1_Joint[1];
+        indexed_joints[16] = LeftLarm_Joint[0];
 
         reward = 0;
         done = 0;
         step = 0;
-        countdown = 1000;
+        countdown = 10;
 
 
         GetPartsStates();
@@ -173,7 +172,7 @@ public class Env : MonoBehaviour
         StockPartsStates();
         GetJointsStates();
         StockJointsStates();
-        // DiffJointsStates();
+        DiffJointsStates();
 
         StockOutputData();
 
@@ -184,45 +183,17 @@ public class Env : MonoBehaviour
         {
             double dRandom = (cRandom.NextDouble() * 2.0) - 1.0;
             float fRandom = (float)dRandom;
-            // if(i != 5 && i != 9){
-            //     actions[i] = 0f; //fRandom; //-1f;
-            // } else {
-            //     actions[i] = -1f; //fRandom; //-1f;
-            // }
-            actions[i] = fRandom;
+            actions[i] = fRandom; //-1f;
         }
+
 
         // HingeJoint General
-        float[] min_limit = new float[num_joints]   {-75, -45, -35, -120, -160, -120, -160, -85, -90, -85, -90}; 
-        float[] max_limit = new float[num_joints]   {30, 45, 35, 20, 2, 20, 2, 60, 50, 60, 50};
         for (int i = 0; i < num_joints; i++)
         {
-            JointLimits[] Limits = new JointLimits[num_joints];
-            Limits[i] = indexed_joints[i].limits;
-            Limits[i].min = min_limit[i];
-            Limits[i].max = max_limit[i];
-            Limits[i].bounciness = 0f;
-            Limits[i].bounceMinVelocity = 0f;
-            indexed_joints[i].limits = Limits[i];
-            indexed_joints[i].useLimits = true;
-            indexed_joints[i].enableCollision = false;
-            // Debug.Log(String.Format("idx{0}: R= {1:f}, V= {2:f}, Axis={3}", i, joints_data[i*2], joints_data[i*2+1], indexed_joints[i].axis));
+            if(i != 5 && i != 9) indexed_joints[i].enableCollision = false;
+            else indexed_joints[i].enableCollision = true;
         }
 
-
-        // HingeJoint Motor
-        JointMotor[] Motors = new JointMotor[num_joints];
-        float[] motor_power = new float[num_joints] {100, 100, 100, 300, 200, 300, 200,  75,  75,  75,  75};
-        for (int i = 0; i < num_joints; i++) //(int i = 5; i < 6; i++)//
-        {
-            Motors[i] = indexed_joints[i].motor;
-            Motors[i].targetVelocity = Math.Sign(actions[i]) * 360f;
-            Motors[i].force = motor_power[i] * 410f * Math.Abs(actions[i]);
-            Motors[i].freeSpin = false;
-            indexed_joints[i].motor = Motors[i];
-            indexed_joints[i].useMotor = true;
-            Debug.Log(String.Format("idx{0}: R= {1:f}, V= {2:f}, Axis={3}", i, joints_data[i*2], joints_data[i*2+1], indexed_joints[i].axis));
-        }
 
         // HingeJoint Spring
         // JointSpring[] Springs = new JointSpring[num_joints];
@@ -233,8 +204,29 @@ public class Env : MonoBehaviour
         //     Springs[i].damper = 10;
         //     Springs[i].targetPosition = init_joints_data[i*2];
         //     indexed_joints[i].spring = Springs[i];
-        //     Debug.Log(String.Format("idx{0}: R= {1:f}, V= {2:f}, Axis={3}", i, joints_data[i*2], joints_data[i*2+1], indexed_joints[i].axis));
+        //     Debug.Log(String.Format("idx{0}: R= {1:f}, V= {2:f}", i, diff_joints_data[i*2], diff_joints_data[i*2+1]));
         // }
+
+
+        // HingeJoint Motor
+        JointMotor[] Motors = new JointMotor[num_joints];
+        for (int i = 0; i < num_joints; i++) Motors[i] = indexed_joints[i].motor;
+
+        float[] motor_power = new float[num_joints] {100, 100, 100, 100, 100, 300,  200,  100, 100, 300,  200,  75,  75,  75,  75,  75,  75};
+        float[] max_limit = new float[num_joints]   {45,  30,  35,  5,   35,  20,   -2,   5,   35,  20,   -2,   60,  60,  50,  85,  85,  50};   
+        float[] min_limit = new float[num_joints]   {-45, -75, -35, -25, -60, -120, -160, -25, -60, -120, -160, -85, -85, -90, -60, -60, -90};         
+
+        for (int i = 0; i < num_joints; i++)
+        {
+            Motors[i].targetVelocity = Math.Sign(actions[i]) * 100f;
+            if (diff_joints_data[i*2] > max_limit[i]) Motors[i].targetVelocity = -100f;
+            else if (diff_joints_data[i*2] < min_limit[i]) Motors[i].targetVelocity = 100f;
+            Motors[i].force = motor_power[i] * 0.41f * Math.Abs(actions[i]);
+            Motors[i].freeSpin = true;
+            indexed_joints[i].useMotor = true;
+            indexed_joints[i].motor = Motors[i];
+            Debug.Log(String.Format("idx{0}: R= {1:f}, V= {2:f}", i, diff_joints_data[i*2], diff_joints_data[i*2+1]));
+        }
 
 
         if ((countdown <= 0) || (stop_flag)) //achieve the goal
@@ -242,7 +234,7 @@ public class Env : MonoBehaviour
             done = 9;
             Quit();
         }
-        else if (step > 1024 || game_objects_y[0] < -0.6f)
+        else if (step > 1024)
         {
             reward += 1;
             done = 1;
@@ -296,7 +288,6 @@ public class Env : MonoBehaviour
             rigid_bodies_wz[i] = rigid_bodies[i].angularVelocity.z;
         }
     }
-
     void StockPartsStates()
     {
         for (int i = 0; i < num_objects; i++) 
@@ -315,7 +306,6 @@ public class Env : MonoBehaviour
             parts_data[i*12+11] = rigid_bodies_wz[i];
         }
     }
-
     void SetInitPartsStates()
     {
         for (int i = 0; i < num_objects; i++) 
@@ -330,27 +320,29 @@ public class Env : MonoBehaviour
         // FloorRB.velocity = new Vector3(0f, 0f, 0f);
         // FloorRB.angularVelocity = new Vector3(0f, 0f, 0f);
     }
-
     float CurrentRelativePosition(HingeJoint hinge_joint)
     {
-        float pos = hinge_joint.angle;
-        if (pos < -180f)
-        {
-            pos += 360f;
-        }       
-        if (pos > 180f)
-        {
-            pos -= 360f;
-        }
-        return pos;
-    }
+            double pos = Vector3.Dot(hinge_joint.axis, hinge_joint.transform.localEulerAngles);
+            if (pos < -180)
+            {
+                pos = pos + 360;
+            }       
+            if (pos > 180)
+            {
+                pos = pos - 360;
+            }
 
+            float pos_ = (float)pos;
+
+    return pos_;
+    }
     float CurrentRelativeVelosity(HingeJoint hinge_joint)
     {
-        float vel = hinge_joint.velocity;
-        return vel;
-    }
+            double vel = hinge_joint.velocity;
+            float vel_ = (float)vel;
 
+    return vel_;
+    }
     void GetJointsStates()
     {
         for (int i = 0; i < num_joints; i++) 
@@ -359,7 +351,6 @@ public class Env : MonoBehaviour
             joint_velocity[i] = CurrentRelativeVelosity(indexed_joints[i]);
         }                                                                                                                                                    
     }
-
     void StockJointsStates()
     {
         for (int i = 0; i < num_joints; i++) 
@@ -368,7 +359,6 @@ public class Env : MonoBehaviour
             joints_data[i*2+1] = joint_velocity[i];
         }
     }
-
     void DiffJointsStates()
     {
         GetJointsStates();
@@ -376,9 +366,19 @@ public class Env : MonoBehaviour
         for(int i = 0; i < num_joints*2; i++)
         {
             diff_joints_data[i] = joints_data[i] - init_joints_data[i];
+
+
+            // if (i % 2 == 0 && diff_joints_data[i] < -180)
+            // {
+            //     diff_joints_data[i] = diff_joints_data[i] + 360;
+            // }       
+            // else if (i % 2 == 0 && diff_joints_data[i] > 180)
+            // {
+            //     diff_joints_data[i] = diff_joints_data[i] - 360;
+            // }
+
         }
     }
-
     void StockOutputData()
     {
         // more
